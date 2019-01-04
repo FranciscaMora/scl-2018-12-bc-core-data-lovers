2038a9933e7f240df512f4c7b77a68d02451b98e
FranciscaMora <fmoraparedes@gmail.com>
2019-01-03 22:19:56 -0300

    all_guardando cambios generales sin mayor relevancia a lo previamente regitrado

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload=()=>{
+    document.getElementById("champion-screen").style.display="none";
+
+
+  
+};
+
+function runes(){
+    document.getElementById("main-screen").style.display="none";
+    document.getElementById("champion-screen").style.display="block";
+}
+
+
+
+// var instance = M.Carousel.getInstance(Carousel);
+// document.addEventListener('DOMContentLoaded', function() {
+//     var instance = M.Carousel.getInstance(Carousel);
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(Carousel, options);
+//   });
