3e4ffb6f3dc9c466852c5a752ec8255153c3409a
LoreMiranda <lorena.miranda.lagos@gmail.com>
2019-01-06 16:06:31 -0300

    CORRECCION se inicia nuevamente el codigo (lo anterior queda comentado)

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
-window.lolData = {
+/*window.lolData = {
 
     filter: (data) => {
 
-        const result = data.LOL.filter(element => {
+        const result = data.lol.filter(element => {
 
-        return element.type.indexOff(condition) !== -1;
+        return element.tags.indexOf(condition) !== -1;
         })
         return result
     },
 
     orderAZ: (data) => {
-        const resultOrden = data.LOL.sort((a, b) => {
+        const resultOrden = data.lol.sort((a, b) => {
             if (a.name > b.name) {
                 return 1;
             } if (a.name < b.name) {
@@ -21,7 +21,7 @@ window.lolData = {
         },
 
     orderZA: (data) => {
-        const resultOrden = data.LOL.sort((a, b) => {
+        const resultOrden = data.lol.sort((a, b) => {
             if (a.name < b.name) {
                 return 1;
             } if (a.name > b.name) {
@@ -31,58 +31,21 @@ window.lolData = {
                 return resultOrden
       }
 }
+*/
 
+const lolData = Object.values(LOL.data);
+const tag = [];
+const stats = [];
+const icon = [];
+const splash = [];
 
-/*Toma el id de los champ */
-let name =[];
-/*Toma la img de champ*/ 
-let icon=[];
-/*Toma la biografia de champ */
-let biography=[];
-/*Toma el titulo de champ */
-let title=[];
-/*Toma los tags */
-let tag=[];
-/*Toma los stats */
-let stats=[];
-/**toma la imagen y ubicación en html */
-let img=[];
-/**Toma el splash para luego ubicarlo en background */
-let splash=[];
+function filter (lolData,condition) {
+   
 
-let arrayLOL= Object.values(LOL.data);
-arrayLOL.forEach(element =>{
-    name.push(element.id);
-    icon.push(element.img);
-    biography.push(element.blurb);
-    title.push(element.title);
-    tag.push(element.tags);
-    stats.push(element.stats);
-    img.push(element.image);
-    splash.push(element.splash);
-});
-
-/**Toma el valor de armor de cada champ*/
-let armor=[];
-/**Toma el daño de ataque de cada champ */
-let attackDamage=[];
-/**Toma el HP */
-let healPoints=[];
-/**Toma los mp */
-let magicPoints=[];
-/**Toma la defenza mágica */
-let magicDefense=[];
-/**Toma rango de ataque */
-let attackRange=[];
-
-let arrayStats= Object.values(stats);
-    arrayStats.forEach(element =>{
-    armor.push(element.armor);
-    attackDamage.push(element.attackdamage);
-    healPoints.push(element.hp);
-    magicPoints.push(element.mp);
-    magicDefense.push(element.spellblock);
-    attackRange.push(element.attackrange);
-
-});
-
+    const tag = lolData.filter(tags =>{
+     return tags.tag.indexOf(condition)!==-1;
+     });
+     console.log(tag)
+     return tag;
+ 
+ }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-window.onload=()=>{
+/*window.onload=()=>{
     document.getElementById("champion-screen").style.display="none";
 
 
@@ -18,3 +18,60 @@ function runes(){
 //     var elems = document.querySelectorAll('.carousel');
 //     var instances = M.Carousel.init(Carousel, options);
 //   });
+
+
+
+/*Toma el id de los champ */
+//let name =[];
+/*Toma la img de champ*/ 
+//let icon=[];
+/*Toma la biografia de champ */
+//let biography=[];
+/*Toma el titulo de champ */
+//let title=[];
+/*Toma los tags */
+//let tag=[];
+/*Toma los stats */
+//let stats=[];
+/**toma la imagen y ubicación en html */
+//let img=[];
+/**Toma el splash para luego ubicarlo en background */
+//let splash=[];
+
+/*let arrayLOL= Object.values(LOL.data);
+arrayLOL.forEach(element =>{
+    name.push(element.id);
+    icon.push(element.img);
+    biography.push(element.blurb);
+    title.push(element.title);
+    tag.push(element.tags);
+    stats.push(element.stats);
+    img.push(element.image);
+    splash.push(element.splash);
+});
+
+/**Toma el valor de armor de cada champ*/
+//let armor=[];
+/**Toma el daño de ataque de cada champ */
+//let attackDamage=[];
+/**Toma el HP */
+//let healPoints=[];
+/**Toma los mp */
+//let magicPoints=[];
+/**Toma la defenza mágica */
+//let magicDefense=[];
+/**Toma rango de ataque */
+//let attackRange=[];
+
+/*let arrayStats= Object.values(stats);
+    arrayStats.forEach(element =>{
+    armor.push(element.armor);
+    attackDamage.push(element.attackdamage);
+    healPoints.push(element.hp);
+    magicPoints.push(element.mp);
+    magicDefense.push(element.spellblock);
+    attackRange.push(element.attackrange);
+
+});
+
+*/
