49782b06567946dea3c05a37fe875d9b6912cb54
FranciscaMora <fmoraparedes@gmail.com>
2019-01-08 16:04:31 -0300

    main.js_se agrega pantalla principal con iconos en card y pantalla runas con iconos en cards

--- src/main.js
+++ src/main.js
@@ -1,23 +1,71 @@
-window.onload=()=>{
-    document.getElementById("champion-screen").style.display="none";
-
-
-  
+window.onload = () => {
+  document.getElementById("champion-screen").style.display = "none";
 };
-
-document.getElementById("runes").addEventListener("click",(evento)=>{
-    evento.preventDefault();
-    document.getElementById("main-screen").style.display="none";
-    document.getElementById("champion-screen").style.display="block";
+document.addEventListener('DOMContentLoaded', function () {
+  M.AutoInit();
 });
 
+/**Pantalla runas */
+const containerRoot2=document.getElementById('root2');
+document.getElementById('runes').addEventListener('click', (evento) => {
+  evento.preventDefault();
+  document.getElementById('main-screen').style.display = 'none';
+  document.getElementById('champion-screen').style.display = 'block';
+  data.forEach(element=>{
+  containerRoot2.innerHTML+=`
+  
+    <div class="col s12 m2">
+      <div class="card">
+        <div class="card-image">
+          <img src=${element.splash}>
+        </div>
+        <div class="card-action">
+          <a href="#">${element.name}</a>
+        </div>
+      </div>
+    </div>
+  
+            
+`
+  })
+});
+
+const containerRoot = document.getElementById('root');
+/**Cambio de tag según valor select */
+document.getElementById('selectTags').addEventListener('change', () => {
+  let rol = document.getElementById('selectTags').value;
+  let filtered = filter(data, rol);
+  console.log(filtered)
+  containerRoot.innerHTML = '';
+  filtered.forEach(element => {
+    containerRoot.innerHTML += ` 
+
+        <div class="col s4 m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="${element.splash}">
+              <span class="card-title">${element.name}</span>
+              <a id="button" data-target="modal1" class="btn-floating halfway-fab waves-effect waves-light red btn modal-trigger"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>${element.title}</p>
+            </div>
+          </div>
+        </div>
+      `
+  })
+
+})
+
+/**Pantalla runas */
 
 
 
+// document.getElementById('button')document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.modal');
+//   var instances = M.Modal.init(elems, options);
 
-
-
-
+// });
 
 
 // var instance = M.Carousel.getInstance(Carousel);
