629f6540c668d4a4d5cae71ef4ec9931f9c1bb5b
FranciscaMora <fmoraparedes@gmail.com>
2019-01-09 10:05:19 -0300

    main.js se agrega dom de filtro que rellena cards

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 window.onload = () => {
   document.getElementById("champion-screen").style.display = "none";
 };
-document.addEventListener('DOMContentLoaded', function () {
-  M.AutoInit();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   M.AutoInit();
+// });
+
+
+
+const containerRoot = document.getElementById('root');
+/**Cambio de tag según valor select */
+document.getElementById('selectTags').addEventListener('change', () => {
+  let rol = document.getElementById('selectTags').value;
+  let filtered = filter(data,rol);
+  containerRoot.innerHTML = '';
+  filtered.forEach(element => {
+    containerRoot.innerHTML += ` <div class="card small col s12 l3">
+  <div class="card-image waves-effect waves-block waves-light">
+    <img class="activator" src=${element.splash}>
+  </div>
+  <div class="card-content">
+  <span class="card-title activator grey-text text-darken-4">${element.name}<i class="material-icons right">more_vert</i></span>
+    <p><a href="#">Builds</a> <a href="#">Runas</a></p>
+  </div>
+  <div class="card-reveal">
+    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+    <p>${element.info}</p>
+  </div>
+</div>
+          
+      `
+  })
+
+})
+
 
 /**Pantalla runas */
 const containerRoot2=document.getElementById('root2');
@@ -30,47 +59,3 @@ document.getElementById('runes').addEventListener('click', (evento) => {
   })
 });
 
-const containerRoot = document.getElementById('root');
-/**Cambio de tag según valor select */
-document.getElementById('selectTags').addEventListener('change', () => {
-  let rol = document.getElementById('selectTags').value;
-  let filtered = filter(data, rol);
-  console.log(filtered)
-  containerRoot.innerHTML = '';
-  filtered.forEach(element => {
-    containerRoot.innerHTML += ` 
-
-        <div class="col s4 m4">
-          <div class="card">
-            <div class="card-image">
-              <img src="${element.splash}">
-              <span class="card-title">${element.name}</span>
-              <a id="button" data-target="modal1" class="btn-floating halfway-fab waves-effect waves-light red btn modal-trigger"><i class="material-icons">add</i></a>
-            </div>
-            <div class="card-content">
-              <p>${element.title}</p>
-            </div>
-          </div>
-        </div>
-      `
-  })
-
-})
-
-/**Pantalla runas */
-
-
-
-// document.getElementById('button')document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.modal');
-//   var instances = M.Modal.init(elems, options);
-
-// });
-
-
-// var instance = M.Carousel.getInstance(Carousel);
-// document.addEventListener('DOMContentLoaded', function() {
-//     var instance = M.Carousel.getInstance(Carousel);
-//     var elems = document.querySelectorAll('.carousel');
-//     var instances = M.Carousel.init(Carousel, options);
-//   });
