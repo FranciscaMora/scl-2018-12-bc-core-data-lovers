5ea16ce95e981b5ac86a3fccf6bc23beafb2a164
FranciscaMora <fmoraparedes@gmail.com>
2019-01-11 08:30:54 -0300

    mainjs__finalizada v1.0

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ window.onload = () => {
 // document.addEventListener('DOMContentLoaded', function () {
 //   M.AutoInit();
 // });
-
+const data1=Object.entries(LOL.data);
 
 
 const containerRoot = document.getElementById('root');
 /**Cambio de tag según valor select */
 document.getElementById('selectTags').addEventListener('change', () => {
   let rol = document.getElementById('selectTags').value;
-  let filtered = filter(data, rol);
+  let filtered = filter(data1, rol);
   containerRoot.innerHTML = '';
   filtered.forEach(element => {
     containerRoot.innerHTML += ` <div class="card small col s4 l3">
@@ -38,38 +38,6 @@ document.getElementById('selectTags').addEventListener('change', () => {
 })
 
 
-document.getElementById('selectOrder').addEventListener('change', (evento) => {
-  let selectValue = document.getElementById('selectOrder').value;
-  containerRoot.innerHTML='';
-  data.forEach(element => {
-    containerRoot.innerHTML +=
-      ` <div class="card small col s12 l3">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src=${element[1].splash}>
-    </div>
-    <div class="card-content">
-    <span class="card-title activator text-darken-4">${element[1].name}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">Builds</a> <a href="#">Runas</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Stats<i class="material-icons right">close</i></span>
-      <p>Hp:${element[1].stats.hp}</p>
-    <p>Mp:${element[1].stats.mp}</p>
-    <p>Defensa física:${element[1].stats.armor}</p>
-    <p>Defensa mágica:${element[1].stats.spellblock}</p>
-    <p>Rango de ataque:${element[1].stats.attackrange}</p>
-    </div>
-  </div>
-            
-        `
-    
-
-    
-  });
-})
-
-
-/**Pantalla runas */
 const containerRoot2 = document.getElementById('root2');
 document.getElementById('runes').addEventListener('click', (evento) => {
   evento.preventDefault();
@@ -96,27 +64,40 @@ document.getElementById('runes').addEventListener('click', (evento) => {
 
 
 
+// manejo de función no completado, se espera su total funcionalidad para la versión 1.1
 
-
-//   let champName = sort(data, data[i].name, selectValue);
-//   for (let i = 0; i < champName.length; i++) {
+// document.getElementById('selectOrder').addEventListener('change', () => {
+//   let selectValue = document.getElementById('selectOrder').value;
+//   function orderAz(sortedByName){
+//   containerRoot.innerHTML = '';
+//   const sortedById= sort(data, data[1].id, selectValue);
+//   sortedByName.forEach(element => {
 //     containerRoot.innerHTML +=
 //       ` <div class="card small col s12 l3">
 //     <div class="card-image waves-effect waves-block waves-light">
-//       <img class="activator" src=${champName[i].splash}>
+//       <img class="activator" src=${element[1].splash}>
 //     </div>
 //     <div class="card-content">
-//     <span class="card-title activator text-darken-4">${champName[i].name}<i class="material-icons right">more_vert</i></span>
+//     <span class="card-title activator text-darken-4">${element[1].name}<i class="material-icons right">more_vert</i></span>
 //       <p><a href="#">Builds</a> <a href="#">Runas</a></p>
 //     </div>
 //     <div class="card-reveal">
-//       <span class="card-title grey-text text-darken-4">Biografía<i class="material-icons right">close</i></span>
-//       <p>${champName[i].blurb}</p>
+//       <span class="card-title grey-text text-darken-4">Stats<i class="material-icons right">close</i></span>
+//       <p>Hp:${element[1].stats.hp}</p>
+//     <p>Mp:${element[1].stats.mp}</p>
+//     <p>Defensa física:${element[1].stats.armor}</p>
+//     <p>Defensa mágica:${element[1].stats.spellblock}</p>
+//     <p>Rango de ataque:${element[1].stats.attackrange}</p>
 //     </div>
 //   </div>
             
 //         `
-//   }
+
+
+
+//   });
+
+// }
 // });
 
 
@@ -125,28 +106,11 @@ document.getElementById('runes').addEventListener('click', (evento) => {
 
 
 
-// const containerRoot3 = document.getElementById('root3');
-// document.getElementById('champ-runes-link').addEventListener('click', (evento) => {
-//   evento.preventDefault();
-//   document.getElementById('runes-screen').style.display = 'none';
-//   document.getElementById('champ-runes').style.display = 'block';
-// });
-//   if (champname - link === element.name) {
-//     containerRoot3.innerHTML = `
-
-//     <div id="champ-card"class="col s12 m2">
-//       <div class="card">
-//         <div class="card-image">
-//           <img src=${element.splash}>
-//         </div>
-//         <div class="card-action">
-//           <a id="champname-link">${element.name}</a>
-//         </div>
-//       </div>
-//     </div>
 
 
-// `
-//   }
+
+
+
+
 
 
