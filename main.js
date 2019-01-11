beded85cecbf1e21ff848ff76f85243b69c032bf
FranciscaMora <fmoraparedes@gmail.com>
2019-01-10 23:49:11 -0300

    main.js_ se agrega dom de filtrado y ordenar de A a Z

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  document.getElementById("champion-screen").style.display = "none";
+  document.getElementById("runes-screen").style.display = "none";
 };
 // document.addEventListener('DOMContentLoaded', function () {
 //   M.AutoInit();
@@ -11,20 +11,24 @@ const containerRoot = document.getElementById('root');
 /**Cambio de tag según valor select */
 document.getElementById('selectTags').addEventListener('change', () => {
   let rol = document.getElementById('selectTags').value;
-  let filtered = filter(data,rol);
+  let filtered = filter(data, rol);
   containerRoot.innerHTML = '';
   filtered.forEach(element => {
-    containerRoot.innerHTML += ` <div class="card small col s12 l3">
+    containerRoot.innerHTML += ` <div class="card small col s4 l3">
   <div class="card-image waves-effect waves-block waves-light">
-    <img class="activator" src=${element.splash}>
+    <img class="activator" src=${element[1].splash}>
   </div>
   <div class="card-content">
-  <span class="card-title activator grey-text text-darken-4">${element.name}<i class="material-icons right">more_vert</i></span>
+  <span class="card-title activator text-darken-4">${element[1].name}<i class="material-icons right">more_vert</i></span>
     <p><a href="#">Builds</a> <a href="#">Runas</a></p>
   </div>
   <div class="card-reveal">
-    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-    <p>${element.info}</p>
+    <span class="card-title grey-text text-darken-4">Stats<i class="material-icons right">close</i></span>
+    <p>Hp:${element[1].stats.hp}+(${element[1].stats.hpperlevel}por nivel)</p>
+    <p>Mp:${element[1].stats.mp}+(${element[1].stats.mpperlevel}por nivel)</p>
+    <p>Defensa física:${element[1].stats.armor}+(${element[1].stats.armorperlevel}por nivel)</p>
+    <p>Defensa mágica:${element[1].stats.spellblock}+(${element[1].stats.spellblockperlevel}por nivel)</p>
+    <p>Rango de ataque:${element[1].stats.attackrange}</p>
   </div>
 </div>
           
@@ -34,22 +38,53 @@ document.getElementById('selectTags').addEventListener('change', () => {
 })
 
 
+document.getElementById('selectOrder').addEventListener('change', (evento) => {
+  let selectValue = document.getElementById('selectOrder').value;
+  containerRoot.innerHTML='';
+  data.forEach(element => {
+    containerRoot.innerHTML +=
+      ` <div class="card small col s12 l3">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src=${element[1].splash}>
+    </div>
+    <div class="card-content">
+    <span class="card-title activator text-darken-4">${element[1].name}<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Builds</a> <a href="#">Runas</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Stats<i class="material-icons right">close</i></span>
+      <p>Hp:${element[1].stats.hp}</p>
+    <p>Mp:${element[1].stats.mp}</p>
+    <p>Defensa física:${element[1].stats.armor}</p>
+    <p>Defensa mágica:${element[1].stats.spellblock}</p>
+    <p>Rango de ataque:${element[1].stats.attackrange}</p>
+    </div>
+  </div>
+            
+        `
+    
+
+    
+  });
+})
+
+
 /**Pantalla runas */
-const containerRoot2=document.getElementById('root2');
+const containerRoot2 = document.getElementById('root2');
 document.getElementById('runes').addEventListener('click', (evento) => {
   evento.preventDefault();
   document.getElementById('main-screen').style.display = 'none';
-  document.getElementById('champion-screen').style.display = 'block';
-  data.forEach(element=>{
-  containerRoot2.innerHTML+=`
+  document.getElementById('runes-screen').style.display = 'block';
+  data.forEach(element => {
+    containerRoot2.innerHTML += `
   
-    <div class="col s12 m2">
+    <div id="champ-card"class="col s12 m2">
       <div class="card">
         <div class="card-image">
           <img src=${element.splash}>
         </div>
         <div class="card-action">
-          <a href="#">${element.name}</a>
+          <a  class="waves-effect waves-teal btn-flat" id="champ-runes-link">${element.name}</a>
         </div>
       </div>
     </div>
@@ -58,4 +93,60 @@ document.getElementById('runes').addEventListener('click', (evento) => {
 `
   })
 });
+
+
+
+
+
+//   let champName = sort(data, data[i].name, selectValue);
+//   for (let i = 0; i < champName.length; i++) {
+//     containerRoot.innerHTML +=
+//       ` <div class="card small col s12 l3">
+//     <div class="card-image waves-effect waves-block waves-light">
+//       <img class="activator" src=${champName[i].splash}>
+//     </div>
+//     <div class="card-content">
+//     <span class="card-title activator text-darken-4">${champName[i].name}<i class="material-icons right">more_vert</i></span>
+//       <p><a href="#">Builds</a> <a href="#">Runas</a></p>
+//     </div>
+//     <div class="card-reveal">
+//       <span class="card-title grey-text text-darken-4">Biografía<i class="material-icons right">close</i></span>
+//       <p>${champName[i].blurb}</p>
+//     </div>
+//   </div>
+            
+//         `
+//   }
+// });
+
+
+
+
+
+
+
+// const containerRoot3 = document.getElementById('root3');
+// document.getElementById('champ-runes-link').addEventListener('click', (evento) => {
+//   evento.preventDefault();
+//   document.getElementById('runes-screen').style.display = 'none';
+//   document.getElementById('champ-runes').style.display = 'block';
+// });
+//   if (champname - link === element.name) {
+//     containerRoot3.innerHTML = `
+
+//     <div id="champ-card"class="col s12 m2">
+//       <div class="card">
+//         <div class="card-image">
+//           <img src=${element.splash}>
+//         </div>
+//         <div class="card-action">
+//           <a id="champname-link">${element.name}</a>
+//         </div>
+//       </div>
+//     </div>
+
+
+// `
+//   }
+
 
